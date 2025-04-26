f666a30c3404f122867577cd792bac55e54d69eb
rushikeshb30 <rushikesh.bhadage30@gmail.com>
2025-04-27 01:02:33 +0530

    Fixed System.err issues and replaced with logger

--- src/main/java/com/dsa360/api/annotatios/ValidFile.java
+++ src/main/java/com/dsa360/api/annotatios/ValidFile.java
@@ -1,16 +1,20 @@
 package com.dsa360.api.annotatios;
-import javax.validation.Constraint;
-import javax.validation.Payload;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
 @Target({ ElementType.FIELD, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = MultipartFileValidator.class)
 public @interface ValidFile {
-    String message() default "Invalid file";
-    Class<?>[] groups() default {};
-    Class<? extends Payload>[] payload() default {};
+	String message() default "Invalid file";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
 }
